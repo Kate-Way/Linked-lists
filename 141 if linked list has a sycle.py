e2dd37a11899e6e4e9d6af492a2fb6455da2f9dd
Kate Wayenberg <106112580+Kate-Way@users.noreply.github.com>
2022-09-26 17:27:55 -0700

    Updated with a faster working solution

--- 141 if linked list has a sycle.py
+++ 141 if linked list has a sycle.py
@@ -9,17 +9,15 @@
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        curr = head
-        curr2 = head
-        if curr == None or curr.next == None:
+        tortoise = head
+        hare = head
+        if tortoise == None or tortoise.next == None:
             return False
-        while curr.next:
-            curr = curr.next
-            curr2 = curr2.next
-            if curr2 == None or curr2.next == None:
+        while tortoise.next:
+            tortoise = tortoise.next
+            hare = hare.next.next
+            if hare == None or hare.next == None:
                 return False
-            else:
-                curr2 = curr2.next
-            if curr == curr2:
+            if tortoise == hare:
                 return True
 
